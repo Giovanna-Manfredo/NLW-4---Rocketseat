26d84d9955a113b0206e2630f4715250b728a342
Giovanna Manfredo <giovanna.cmanfredo@gmail.com>
2021-02-25 18:45:15 -0300

    first comit

---
+++ src/Components/experienceBar.tsx
@@ -0,0 +1,18 @@
+export function ExperienceBar(){
+    return(
+        <header className="experience_bar">
+            <span>
+                0 xp
+            </span>
+                <div>
+                    <div style={{width: '60%'}}/>
+                    <span className="current-experience" style={{left: '50%'}}>
+                        300xp
+                        </span> 
+                </div>
+            <span>
+                600 xp
+            </span>
+        </header>
+    );
+} 
